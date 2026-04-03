7b80bc51a242d5c60d6c7769546479d11381a94c
naming404 <naming404@users.noreply.github.com>
2026-04-03 15:31:32 +0000

    deploy: 3ddd8a2eb7f56ff3792567b1365175d587f76678

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import { gsap } from 'gsap'
-import { ScrollTrigger } from 'gsap/ScrollTrigger'
-import App from './App.vue'
-import './assets/styles/main.css'
-
-gsap.registerPlugin(ScrollTrigger)
-
-createApp(App).mount('#app')
--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-export default defineConfig({
-  plugins: [vue()],
-  // Change base to '/portfolio/' if deploying to huunghia2807.github.io/portfolio
-  base: './',
-})
